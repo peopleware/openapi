e8ce04f9c6f0d598de4501ae52a12c6731ef6fbc
Jan Dockx <Jan_Dockx@peopleware.be>
2022-06-29 12:25:05 +0200

    fmt

--- test/health/Status.test.js
+++ test/health/Status.test.js
@@ -17,7 +17,7 @@
 const testName = require('../../_util/_testName')
 const shouldBeSeriousSchema = require('../../_util/_shouldBeSeriousSchema')
 const { stuff } = require('../../_util/_stuff')
-const {Status} = require('../../health/Status')
+const { Status } = require('../../health/Status')
 
 describe(testName(module), function () {
   shouldBeSeriousSchema(Status, stuff)
