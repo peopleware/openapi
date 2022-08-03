49ad8b42244234d9aa9afa3735e4eae0ffa6081d
Jan Dockx <Jan_Dockx@peopleware.be>
2022-08-03 15:21:20 +0200

    add Prefix, RAX, and RAA schemata and tests

---
+++ oauth2/stsWithRAA/JWT/RAA2.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 – 2022 PeopleWare
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+const Joi = require('joi')
+const { Prefix } = require('./Prefix')
+const { RAX } = require('./RAX')
+
+const example = {
+  '/openapi/common/v1': '*:/a/path/*/to/{a,multiple}/resources/**',
+  '/openapi/other/v4': '{GET,PUT}:/another/path/43/without/choices',
+  '/openapi/other/v4/another/path/43/without': 'POST:'
+}
+
+const RAA2 = Joi.object()
+  .pattern(Prefix.required(), RAX.required())
+  .unknown(false)
+  .min(1)
+  .example(example)
+
+module.exports = { raa2Examples: [example], RAA2 }
---
+++ oauth2/stsWithRAA/JWT/RAX.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 – 2022 PeopleWare
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+const Joi = require('joi')
+const addExamples = require('../../../_util/addExamples')
+
+const methodGlobPattern = /^[A-Z*{},]+$/
+const pathGlobPattern = /^[\w-.~%!$&'()+;=*{},/]+$/ // URI segment characters, plus '*', '{', ',', '}', and the '/' separator
+
+const raxExamples = ['*:/a/path/*/to/{a,multiple}/resources/**', '{GET,PUT}:/another/path/43/without/choices', 'POST:']
+
+const RAX = Joi.string()
+  .trim()
+  .custom((value, { error }) => {
+    const [methodGlob, pathGlob] = value.split(':')
+    if (!methodGlobPattern.test(methodGlob) || (pathGlob !== '' && !pathGlobPattern.test(pathGlob))) {
+      return error('any.invalid')
+    }
+    /* TODO
+       - no more than 2 '*' in a row
+       - no '**' in method glob
+       - no consecutive ','
+       - balanced '{…}'
+       - no ',' outside '{…}'
+       - empty option must be the first `{,…} */
+
+    return value
+  }, 'RAX')
+
+module.exports = { methodGlobPattern, pathGlobPattern, raxExamples, RAX: addExamples(RAX, raxExamples) }
