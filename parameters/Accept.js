a92b550e30208078ec4f5c2455dc89fbed286448
Jan Dockx <Jan_Dockx@peopleware.be>
2022-08-08 15:26:55 +0200

    add schema for Accept header parameter

---
+++ parameters/Accept.js
@@ -0,0 +1,19 @@
+const Joi = require('joi')
+const addExamples = require('../_util/addExamples')
+
+const acceptExamples = ['text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8', 'application/json']
+
+/**
+ * See [Regex to match accept-language header](https://javascript.tutorialink.com/regex-to-match-accept-language-header/)
+ *
+ * @type {Joi.StringSchema}
+ */
+const Accept = Joi.string()
+  .pattern(/([^-;]*)(?:-([^;]*))?(?:;q=([0-9].[0-9]))?/)
+  .allow('')
+  .description(
+    'request header that says what mime-types the client wants to be returned, where `q` expresses the preferences'
+  )
+  .label('accept')
+
+module.exports = { acceptExamples, Accept: addExamples(Accept, acceptExamples) }
