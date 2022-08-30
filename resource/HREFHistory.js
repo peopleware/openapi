7b307b2f6635fbabf207b58c53f6a808541f4141
Jan Dockx <Jan_Dockx@peopleware.be>
2022-08-30 10:35:11 +0200

    add description and alteration to HREFHistory

--- resource/HREFHistory.js
+++ resource/HREFHistory.js
@@ -1,12 +1,16 @@
 const Joi = require('joi')
 const { RelativeURI } = require('../string/RelativeURI')
 const addExamples = require('../_util/addExamples')
+const { readOnlyAlteration } = require('./_readOnlyAlteration')
 
 const HREFHistory = Joi.object({
   history: RelativeURI.description(
     "relative URI at which the list of times at which a change in the resource's information was recorded can be retrieved"
   ).required()
-}).unknown(true)
+})
+  .description('HATEOAS links. These are relative URIs.')
+  .alter(readOnlyAlteration)
+  .unknown(true)
 
 const hrefHistoryExamples = [{ history: '1567889875/history' }]
 
