9e0ef96a3eb7d06d0221452a3a2aa3ae3030338d
Jan Dockx <Jan_Dockx@peopleware.be>
2022-09-26 16:01:49 +0200

    define SearchDocument schema and test (with 1 negative failure)

---
+++ resource/SearchDocument.js
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2021 – 2021 PeopleWare
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+const Joi = require('joi')
+const { StructureVersioned, structureVersionedExamples } = require('./StructureVersioned')
+const addExamples = require('../_util/addExamples')
+const { SearchResultBase, searchResultBaseExamples } = require('./SearchResultBase')
+
+const SearchTerm = Joi.string()
+  .trim()
+  .min(1)
+  .description(`free text on which the resource can be found (trimmed, not empty)`)
+
+const SearchDocument = StructureVersioned.append({
+  exact: Joi.array()
+    .items(SearchTerm.example('0123456789'))
+    .unique()
+    .description(
+      `List of strings on which the resource can be found with exact or fuzzy match.
+
+The order is irrelevant. May be empty.`
+    )
+    .example(['0123456789', '9876543210'])
+    .required(),
+  fuzzy: Joi.array()
+    .items(SearchTerm.example('find me'))
+    .unique()
+    .description(
+      `List of strings on which the resource can be found with fuzzy match (together with the list of strings for exact match).
+
+The order is irrelevant. May be empty.`
+    )
+    .example(['find me', 'if you can'])
+    .required(),
+  content: SearchResultBase.required()
+}).description(`Wrapper around the search result (which is returned to the client when the resource is found), with
+information for a search index.
+
+It contains strings for which the resource this is a search document for can be found by, and the \`content\` that is to
+be sent to the client. The resource this is a search document for can be found with an exact match on the strings in
+\`exact\`, and by a fuzzy search on the strings in \`fuzzy\` and \`exact\`.
+
+\`fuzzy\` or \`exact\` may be empty, but not both.
+
+Search results can be limited to selected types with an exact match on \`content.discriminator\`. The found resource
+can be retrieved in the indexed version at \`content.href\`.`)
+
+const searchDocumentExamples = structureVersionedExamples.map(svd => ({
+  ...svd,
+  exact: ['0123456789', '9876543210'],
+  fuzzy: ['find me', 'if you can'],
+  content: searchResultBaseExamples[0]
+}))
+
+module.exports = {
+  searchDocumentExamples,
+  SearchDocument: addExamples(SearchDocument, searchDocumentExamples)
+}
---
+++ test/resource/SearchDocument.test.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 – 2021 PeopleWare
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+const testName = require('../../_util/_testName')
+const shouldBeSeriousSchema = require('../../_util/_shouldBeSeriousSchema')
+const { stuff, stuffWithUndefined } = require('../../_util/_stuff')
+const { SearchDocument, searchDocumentExamples } = require('../../resource/SearchDocument')
+
+function notAnEmptyArray (s) {
+  return !Array.isArray(s) || s.length > 0
+}
+
+function notATrimmedString (s) {
+  return typeof s !== 'string' || s.trim() !== s
+}
+
+describe(testName(module), function () {
+  shouldBeSeriousSchema(
+    SearchDocument,
+    stuff
+      .concat(stuffWithUndefined.filter(notAnEmptyArray).map(exact => ({ ...searchDocumentExamples[0], exact })))
+      .concat(stuffWithUndefined.filter(notATrimmedString).map(eg => ({ ...searchDocumentExamples[0], exact: [eg] })))
+      .concat(stuffWithUndefined.filter(notAnEmptyArray).map(fuzzy => ({ ...searchDocumentExamples[0], fuzzy })))
+      .concat(stuffWithUndefined.filter(notATrimmedString).map(eg => ({ ...searchDocumentExamples[0], fuzzy: [eg] })))
+      .concat(stuffWithUndefined.map(content => ({ ...searchDocumentExamples[0], content })))
+      .concat({ ...searchDocumentExamples[0], exact: [], fuzzy: [] })
+  )
+})
