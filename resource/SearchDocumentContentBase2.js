e69b83cfe05b45e52e5e50c6c47684d859722a02
Jan Dockx <Jan_Dockx@peopleware.be>
2023-01-27 10:11:41 +0100

    sketch SearchResult(s)2

--- resource/SearchDocumentContentBase2.js
+++ resource/SearchDocumentContentBase2.js
@@ -20,6 +20,9 @@ const { StructureVersioned, structureVersionedExamples } = require('./StructureV
 const addExamples = require('../_util/addExamples')
 const { RelativeURI } = require('../string/RelativeURI')
 const { StructureVersion } = require('./StructureVersion')
+const { CanonicalURI } = require('../string/CanonicalURI')
+const { CanonicalURIWithKnowledgeTime } = require('../string/CanonicalURIWithKnowledgeTime')
+const { extendDescription } = require('../_util/extendDescription')
 
 const searchDocumentContentBase2Examples = [
   {
@@ -28,6 +31,21 @@ const searchDocumentContentBase2Examples = [
   }
 ]
 
+const searchResultBase2Examples = [
+  {
+    ...searchDocumentContentBase2Examples[0],
+    href: '/my-service/v6/some-resource/564845?at=2021-01-19T17:14:18.482558Z'
+  }
+]
+
+const searchResultBaseTailoring = 'searchResult'
+
+/**
+ * Included in service search documents as `content`, and used in search results returned by the search service. In the
+ * latter case, the property `href` is added, via the tailoring `searchResult`.
+ *
+ * @type {Joi.Schema<{structureVersion: number, discriminator: string}>}
+ */
 const SearchDocumentContentBase2 = addExamples(
   StructureVersioned.append({
     structureVersion: StructureVersion.valid(Joi.override, 2)
@@ -47,14 +65,37 @@ name clashes (slash-separated).`
       .example('my-service/some-resource')
       .required()
   }).description(
-    `The resource's search document with the most up-to-date information, i.e., up-to-date up until, but not including, \`x-date\`.
+    `The resource's search document with the most up-to-date information, i.e., up-to-date up until, but not including,
+\`x-date\`.
 
-This is a contract between this service and clients of the search service. Specific properties are added for different \`discriminators\`.`
+This is a contract between this service and clients of the search service. Specific properties are added for different
+\`discriminators\`. When returned by the search service, the property \`href\` is added. This is the canonical URI of
+the found resource, including an \`at\` query parameter, that refers to the precise version of the found resource that
+was indexed.`
   ),
   searchDocumentContentBase2Examples
-)
+).alter({
+  [searchResultBaseTailoring]: schema =>
+    addExamples(
+      schema.append({
+        href: extendDescription(
+          CanonicalURIWithKnowledgeTime,
+          `Canonical URI where the found resource's information is located. The \`at\` parameter _must_ be the
+same as the value of the \`x-date\` response header.
+
+Users need to be directed to the version returned by the search index, and not an earlier or more recent
+version. The search engine updates, eventually, after a few seconds. If a more recent version is available in
+the meantime, the user interface makes it possible for the user to navigate to that version.`
+        ).required()
+      }),
+      searchResultBase2Examples
+    )
+})
 
 module.exports = {
   searchDocumentContentBase2Examples,
-  SearchDocumentContentBase2
+  searchResultBase2Examples,
+  SearchDocumentContentBase2,
+  searchResultBaseTailoring,
+  SearchResultBase2: SearchDocumentContentBase2.tailor(searchResultBaseTailoring)
 }
---
+++ resource/searchResults/SearchResults2.js
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2021 – 2021 PeopleWare
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+const Joi = require('joi')
+const addExamples = require('../../_util/addExamples')
+const { SearchResultBase } = require('../SearchResultBase')
+const { SearchDocumentContentBase2, SearchResultBase2 } = require('../SearchDocumentContentBase2')
+
+const resultsExamples = [
+  [
+    {
+      structureVersion: 1,
+      discriminator: 'companies/company',
+      href: '/companies/v1/company/5646897945?at=2021-01-19T17:14:18.482885Z',
+      crn: '5646897945',
+      name: {
+        nl: 'Het Bedrijf',
+        fr: 'La Compagnie'
+      }
+    },
+    {
+      structureVersion: 2,
+      discriminator: 'persons/person',
+      href: '/persons/v1/person/6908390?at=2021-01-19T17:14:18.482558Z',
+      inss: '96110505648',
+      firstName: 'Anna',
+      lastName: 'Van Deuren'
+    }
+  ],
+  []
+]
+
+const MixedSearchResults = Joi.alternatives().try(SearchResultBase, SearchResultBase2)
+
+const Results = addExamples(
+  Joi.array()
+    .items(MixedSearchResults)
+    .max(100)
+    .unique('href')
+    .description(
+      `List of items that match the search, ordered by relevance. The list has \`per_page\` or fewer
+items.
+
+Items identify the type of resource they represent (\`discriminator\`), a link to the indexed
+version of the found resource (\`href\`), and information for humans to recognize the found
+resource. The entries can have different \`structureVersions\`, even for the same \`discriminator\`.
+
+The list has the requested number of items, except for the first and last page. The \`href.first\`
+page can be empty, or have fewer elements than \`per_page\`. The \`href.last\` page can have fewer
+reference in the response than \`per_page\`, but cannot be empty. Other pages have exactly the
+\`per_page\` as number of items.`
+    ),
+  resultsExamples
+)
+
+module.exports = {
+  resultsExamples,
+  Results
+}
