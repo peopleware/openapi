e71c000a9b0c502c0aa28da9c646c1a9269dbb16
Jan Dockx <Jan_Dockx@peopleware.be>
2023-05-04 11:08:32 +0200

    correct and tweak text, add more warnings

--- money/MonetaryValue.js
+++ money/MonetaryValue.js
@@ -45,7 +45,7 @@ the decimal point to the left. A negative number means adding zeros at the end.`
       .example(0)
       .example(-456568)
       .description(
-        `The represented amount of money, multiplied by 10<sup>factor</sup>. This can be positive, negative, or
+        `The represented amount of money, multiplied by 10<sup>decimals</sup>. This can be positive, negative, or
 0. The intepretation of the sign is context dependent.`
       )
   }),
@@ -55,10 +55,9 @@ the decimal point to the left. A negative number means adding zeros at the end.`
   .description(
     `Representation of an \`amount\` of money, in the given \`currency\`, with the given \`decimals\`.
 
-
 The \`amount\` of money is always expressed as an integer. The monetary value it represents is
-<code>amount&nbsp;/&nbsp;10<sup>decimals</sup></code> of the given \`currency\`.
-
+<code>amount.10<sup>&#8239;‑decimals</sup></code> of the given \`currency\`. E.g.,
+\`{"currency": "EUR", "decimals": 4, "amount": 88584439}\` represents €&nbsp;8&nbsp;858,4439.
 
 Calculation with money amounts **MUST** be exact. Only addition (which implies multiplication with integers) and
 subtraction is safe, but multiplication with non-integers (which implies division) and other operations are not. With
@@ -68,13 +67,34 @@ divide €&nbsp;10,00 amongst 3 people, we must decide that we will work with a 
 €&nbsp;3,33, €&nbsp;3,33, and _€&nbsp;3,34_, or, alternatively, hand out €&nbsp;3,33 3 times, and decide what to do with
 the remaining _€&nbsp;0,01_.
 
-
 Money amounts **MUST** be transported and calculated with as integers, and not as floats or doubles, because some
 decimal fractions, e.g., \`0.1\`, _cannot be represented as binary 32- or 64-bit floating point numbers_. If we intend
 €&nbsp;0.1 as monetary value, and we represent the amount as \`0.1\` in JSON, it will be interpreted on reception as a
 number close to, but not equal to \`0.1\`. If we want to calculate 3 times the monetary value €&nbsp;0.1, and we execute
 \`0.1 + 0.1 + 0.1\` or \`3 * 0.1\`, we get \`0.30000000000000004 ≠ 0.3\`. When we instead represent the monetary value
-as ¢&nbsp;10, there is no issue.`
+as ¢&nbsp;10, there is no issue.
+
+The largest \`amount\` that can be represented is
+\`Number.MAX_SAFE_INTEGER\` = 9&nbsp;007&nbsp;199&nbsp;254&nbsp;740&nbsp;991, and the smallest is
+\`Number.MIN_SAFE_INTEGER\` = -9&nbsp;007&nbsp;199&nbsp;254&nbsp;740&nbsp;991 (~&nbsp;±9∙10<sup>15</sup>). With 4
+decimals, this could represent €&nbsp;900&nbsp;719&nbsp;925&nbsp;474,0991 ~ €&nbsp;900&nbsp;billion.
+
+Note that, for addition, all terms must be converted to a representation with the largest \`decimal\` value of all
+terms:
+
+<pre>
+
+{"currency": "EUR", "decimals": 4, "amount": 88584439} +
+
+    {"currency": "EUR", "decimals": 2, "amount": 89418456}
+
+= {"currency": "EUR", "decimals": 4, "amount": 88584439} +
+
+    {"currency": "EUR", "decimals": 4, "amount": 8941845600}
+
+= {"currency": "EUR", "decimals": 4, "amount": 9030430039}
+
+</pre>`
   )
 
 module.exports = { monetaryValueExamples, MonetaryValue }
